08527dd3dfd3de2d73ccb5480ffe5ae6fa2bdff1
emayom <emayom7@gmail.com>
2024-01-22 00:05:35 +0900

    :technologist: 236A - Boy or Girl

--- README.md
+++ README.md
@@ -14,3 +14,4 @@
 | 112A | [petya-and-strings](./problems/petya-and-strings) | 2024-01-21 | 
 | 339A | [helpful-maths](./problems/helpful-maths) | 2024-01-21 | 
 | 281A | [word-capitalization](./problems/word-capitalization) | 2024-01-21 | 
+| 236A | [boy-or-girl](./problems/boy-or-girl) | 2024-01-21 | 
---
+++ problems/boy-or-girl/main.js
@@ -0,0 +1,29 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => (inputString += inputStdin));
+
+process.stdin.on("end", () => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => string.trim());
+
+  main();
+});
+
+function readline() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  const line = readline();
+  const gender = new Set([...line]).size % 2 === 0;
+
+  if (gender) console.log("CHAT WITH HER!");
+  else console.log("IGNORE HIM!");
+}
